3fdee7d272b62056a87e42490220665c6dd57616
Igor-Vallim <igor.vallim@evosystems.com.br>
2022-01-27 18:12:35 -0300

    Commit inicial do projeto

---
+++ Infrastructure/Contexts/TerosContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Contexts
+{
+    public class TerosContext: DbContext
+    {
+        public TerosContext(DbContextOptions<TerosContext> options) : base(options) { }
+    }
+}
---
+++ Repository/Bindings/RepositoryBindings.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Bindings
+{
+    public class RepositoryBindings
+    {
+        public static void Configure(IServiceCollection services)
+        {
+        }
+    }
+}

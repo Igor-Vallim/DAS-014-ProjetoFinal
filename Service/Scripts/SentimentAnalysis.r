caa0cf1a6a02bfa2bb9828174076975c63f8076d
Igor Vallim <igor.vallim@evosystems.com.br>
2022-05-10 21:09:34 -0300

    Update R script

--- Service/Scripts/SentimentAnalysis.r
+++ Service/Scripts/SentimentAnalysis.r
@@ -8,6 +8,8 @@ library(wordcloud)
 library("base64enc")
 library("openssl")
 library("httpuv")
+library(syuzhet)
+library('RPostgres')
 
 
 download.file(url="http://curl.haxx.se/ca/cacert.pem",
@@ -24,15 +26,37 @@ setup_twitter_oauth(api_key,
                     access_secret
 )
 
+movieId <- 10
+
+#Connect to database
+drv <- RPostgres::Postgres()
+
+connec <- dbConnect(drv, 
+                    dbname = "DAS-014",
+                    host = "localhost", 
+                    port = 5432,
+                    user = "postgres",
+                    password = "teste1234")
 
 # Get, for instance, tweets about covid
-
-s <- searchTwitter('covid', lang="en", n=100)
-
+s <- searchTwitter('Scream', lang="en", n=100)
 
 # Converting the query results for a data frame
 df <- do.call("rbind", lapply(s, as.data.frame))
 
+query <- 'INSERT INTO "Tweets"(
+	"Text", "Favorited", "FavoriteCount", "Created", "ScreenName", "RetweetCount", "IsRetweet", "Language", "MovieId")
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);'
+
+#Insert the tweets on the database
+for(record in rownames(df)){
+  
+  values <- c(df[record, "text"], df[record, "favorited"], df[record, "favoriteCount"],
+              "2022-05-10", df[record, "screenName"], df[record, "retweetCount"],
+              df[record, "isRetweet"], "en", movieId)
+  
+  dbSendQuery(connec, query, values)
+}
 
 # Get only tweets
 r_twitter_text <- sapply(s, function(x) x$getText())
@@ -42,9 +66,11 @@ corpus <- Corpus(VectorSource(r_twitter_text))
 
 # Convert text to lowercase
 corpus <- tm_map(corpus, content_transformer(tolower))
+
 # Remove links from corpus
 removeURL <- function(x) gsub("(http.*)", "", x, perl=TRUE)
 corpus <- tm_map(corpus, content_transformer(removeURL))
+
 #cRemove stopwords
 corpus <- tm_map(corpus, removeWords, stopwords("english"))
 
@@ -52,46 +78,27 @@ corpus <- tm_map(corpus, removeWords, stopwords("english"))
 tdm <- TermDocumentMatrix(corpus)
 tdm <- as.matrix(tdm)
 
-# Bar plot
-w <- rowSums(tdm)
-w <- subset(w, w>=10)
-barplot(w,
-        las = 2,
-)
-
-#Plot the results
-w <- sort(rowSums(tdm), decreasing = TRUE)
-set.seed(666)
-#wordcloud(words = names(w),
-#          freq = w,
-#          max.words = 150,
-#          random.order = F,
-#          min.freq = 5,
-#          colors = brewer.pal(8, 'Dark2'),
-#          scale = c(5, 0.3),
-#          rot.per = 0.7)
-
-
 # Sentimental analysis
-
-library(syuzhet
-
-# emotions and sentiments
 sentimental <- get_nrc_sentiment(r_twitter_text, 
                                  lang = "english"
 )
-head(sentimental)
 
-names(sentimental) <- c("raiva", "antecipação", "nojo", "medo", "alegria", 
-                        "tristeza", "surpresa", "confiança", "negativo", 
-                        "positivo")
+names(sentimental) <- c("anger", "anticipation", "disgust", "fear", "joy", 
+                        "sadness", "surprise", "trust", "negative", 
+                        "positive")
+
+sums <- colSums(sentimental)
+
+query <- 'INSERT INTO "Sentiments"(
+	"Date", "Anger", "Anticipation", "Disgust", "Fear", "Joy", "Sadness", "Surprise", "Trust", "Negative", "Positive", "MovieId")
+	VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);'
+
+#Insert the sentiments on the database
+values <- c("2022-05-10", sums[["anger"]], sums[["anticipation"]], sums[["disgust"]],
+            sums[["fear"]], sums[["joy"]], sums[["sadness"]], sums[["surprise"]],
+            sums[["trust"]], sums[["negative"]], sums[["positive"]], movieId)
+  
+dbSendQuery(connec, query, values)
 
 
-#barplot(colSums(sentimental),
-#        las = 2,
-#        ylab = 'Count',
-#        main = 'Sentiment Scores Tweets')
-
-
-nrc   = get_sentiment(r_twitter_text, method = "nrc", lang = "portuguese")
 
